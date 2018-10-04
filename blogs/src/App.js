9a676f9aa487f63749b96a31d350f4f6998ae5a9
Jonathan Ocampo <jalexander1819@gmail.com>
2018-10-04 16:20:20 -0500

    Se realizan modificaciones iniciales para comenzar el proyecto

--- blogs/src/App.js
+++ blogs/src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import * as routes from './constants/routes';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+/* Components */
+import ListContainer from './features/list/container/list';
+import DetailsContainer from './features/details/components/details';
+
+import './assets/App.scss';
+
+const App = () =>
+  (<Router>
+     <div>
+        <Route exact path={routes.MAIN} component={() => <ListContainer />} />
+        <Route exact path={routes.POST_DETAILS} component={() => <DetailsContainer />} />
       </div>
-    );
-  }
-}
+  </Router>);
 
 export default App;
--- blogs/src/index.js
+++ blogs/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import configureStore from './redux/store/configureStore';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
